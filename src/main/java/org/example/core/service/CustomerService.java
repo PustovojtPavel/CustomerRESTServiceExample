8049e9695c35e7def0a39f412b28c5367f72a5b7
Пустовойт Павел Александрович <pustovojt.pavel@gmail.com>
2016-11-19 01:27:46 +0300

    Added new implementation

--- src/main/java/org/example/core/service/CustomerService.java
+++ src/main/java/org/example/core/service/CustomerService.java
@@ -3,7 +3,10 @@ package org.example.core.service;
 import org.example.core.domain.Customer;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<Customer> getAll();
+    Customer findById(UUID customerId);
+    void create(String name);
 }
--- src/main/java/org/example/core/service/CustomerServiceImpl.java
+++ src/main/java/org/example/core/service/CustomerServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -15,5 +16,17 @@ public class CustomerServiceImpl implements CustomerService {
 
     public List<Customer> getAll() {
         return customerDAO.getAll();
+    }
+
+    public Customer findById(UUID customerId) {
+        return customerDAO.findById(customerId);
+    }
+
+    @Override
+    public void create(String name) {
+        Customer customer = new Customer();
+        customer.setId(UUID.randomUUID());
+        customer.setName(name);
+        customerDAO.insert(customer);
     }
 }
